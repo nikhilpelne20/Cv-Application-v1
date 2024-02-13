4c3bb82d6306a5c50c3a205c53dd2350acd3e292
mr-Bones3108 <nikhil788p@gmail.com>
2024-02-13 19:21:49 +0530

    styled header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-function App() {
+import Main from "./components/Main";
 
+function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Main />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CVForm from "./CVForm/CVForm";
+
+export default function Main() {
+  return (
+    <div>
+      <CVForm />
+    </div>
+  );
+}
