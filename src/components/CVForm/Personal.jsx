c873c46c5dd92def43f37d699cb45d449e75c2a2
mr-Bones3108 <nikhil788p@gmail.com>
2024-02-16 09:25:00 +0530

    fix lastname issue

--- src/components/CVForm/Personal.jsx
+++ src/components/CVForm/Personal.jsx
@@ -4,6 +4,7 @@ import Input from '../Utils/Input'
 import TextArea from '../Utils/TextArea'
 
 export default function Personal({onChange,personalInfo}) {
+    console.log(personalInfo.lastName)
   return (
     <Section title="Personal Information">
         <Input
--- src/components/CVPreview/HeaderPre.jsx
+++ src/components/CVPreview/HeaderPre.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HeaderPre({personalInfo}) {
   return (
     <div className='p-3 bg-blue-900 text-white col-span-3 h-[28mm]'>
-        <h1 className='text-5xl font-semibold'>{personalInfo.firstName}</h1>
+        <h1 className='text-5xl font-semibold'>{personalInfo.firstName} {personalInfo.lastName}</h1>
         <p className='text-xl'>{personalInfo.title}</p>
     </div>
   )
