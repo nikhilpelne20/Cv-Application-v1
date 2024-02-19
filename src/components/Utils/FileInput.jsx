e063afb6cba31593848d8252123d055aae1a7755
mr-Bones3108 <nikhil788p@gmail.com>
2024-02-19 22:27:17 +0530

    fixed FileInput style

--- src/components/Utils/FileInput.jsx
+++ src/components/Utils/FileInput.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function FileInput({ onChange, label, name }) {
   return (
-<label className="border-2 border-gray-300 p-1 mb-1 rounded">
-    <input type="file" onChange={onChange} name={name}></input>
+<label className="border-2 border-gray-300 p-1 mb-1 rounded bg-white text-gray-400">
+    <input className="hidden" type="file" onChange={onChange} name={name}></input>
     {label}
 </label>
   );
